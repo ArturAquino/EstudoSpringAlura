3504db0a9dee24298ec7241c255968a977047c4a
ArturAquino <uchihaartur@yahoo.com.br>
2019-12-01 21:41:21 -0300

    Using RestController

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,17 +3,25 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
+// import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+/**
+ * Quando se utiliza a notação '@RestController'
+ * o Spring já entende que tudo que vier como retorno
+ * do controller será um responseBody, logo pode-se
+ * remover a notação '@ResponseBody'.
+ */
 
-@Controller
+// @Controller
+@RestController
 public class TopicosController {
 	
-	@ResponseBody
+	// @ResponseBody
 	@RequestMapping("/topicos")
 	public List<Topico> lista() {
 		Topico topico = new Topico("Duvida", "Duvida sobre Spring", new Curso("Spring", "Programacao"));
